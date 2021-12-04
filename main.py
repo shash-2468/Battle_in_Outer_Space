ec7c84c7fdc9d46a041d113c367e232b3275f29a
shash-2468 <shashanksuroju23@gmail.com>
2021-12-05 00:13:39 +0530

    almost done

--- main.py
+++ main.py
@@ -15,7 +15,19 @@ player = pg.image.load("images/spaceship.png")
 player_x = 380
 player_y = 520
 direction = 0.0
+
+# score
 score = 0
+font = pg.font.Font('freesansbold.ttf', 32)
+go_font = pg.font.Font('freesansbold.ttf', 64)
+
+score_x = 10
+score_y = 10
+
+
+def display_score(x, y):
+    s = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(s, (x, y))
 
 
 def player_fn(x, y):
@@ -38,21 +50,33 @@ def attack(x, y):
 
 def touch(bx, by, ex, ey):
     dist = math.sqrt(math.pow(bx - ex, 2) + math.pow(by - ey, 2))
-    if dist < 20:
+    if dist < 28:
         return True
     return False
 
 
+def game_over():
+    s = go_font.render("GAME OVER", True, (255, 0, 0))
+    screen.blit(s, (230, 220))
+
+
 # enemy
-enemy = pg.image.load("images/enemy.png")
-enemy_x = random.randint(0, 736)
-enemy_y = random.randint(64, 150)
-ex_dir = 0.3
-ey_dir = 0.0
+total = 5
+enemy = []
+enemy_x = []
+enemy_y = []
+ex_dir = []
+ey_dir = []
+for i in range(total):
+    enemy.append(pg.image.load("images/enemy.png"))
+    enemy_x.append(random.randint(0, 736))
+    enemy_y.append(random.randint(64, 150))
+    ex_dir.append(0.4)
+    ey_dir.append(0.0)
 
 
-def enemy_fn(x, y):
-    screen.blit(enemy, (x, y))
+def enemy_fn(x, y, i):
+    screen.blit(enemy[i], (x, y))
 
 
 # Game
@@ -85,15 +109,32 @@ while run:
         player_x = 736
 
     # enemy
-    enemy_x += ex_dir
-    if enemy_x <= 0:
-        ex_dir *= -1
-        enemy_x = 0
-        enemy_y += 30
-    if enemy_x >= 736:
-        ex_dir *= -1
-        enemy_x = 736
-        enemy_y += 30
+    for i in range(total):
+        if enemy_y[i] > 400:
+            for j in range(total):
+                enemy_y[j] = 1000
+            game_over()
+            break
+
+        enemy_x[i] += ex_dir[i]
+        if enemy_x[i] <= 0:
+            ex_dir[i] *= -1
+            enemy_x[i] = 0
+            enemy_y[i] += 30
+        if enemy_x[i] >= 736:
+            ex_dir[i] *= -1
+            enemy_x[i] = 736
+            enemy_y[i] += 30
+        # checking whether the bullet had hit the target
+        hit = touch(bullet_x, bullet_y, enemy_x[i], enemy_y[i])
+        if hit:
+            bullet_y = 520
+            bullet_state = "ready"
+            score += 1
+            print(score)
+            enemy_x[i] = random.randint(0, 736)
+            enemy_y[i] = random.randint(64, 150)
+        enemy_fn(enemy_x[i], enemy_y[i], i)
 
     # If attack
     if bullet_y <= 0:
@@ -104,16 +145,6 @@ while run:
         attack(bullet_x, bullet_y)
         bullet_y -= b_move
 
-    # checking whether the bullet had hit the target
-    hit = touch(bullet_x, bullet_y, enemy_x, enemy_y)
-    if hit:
-        bullet_y = 520
-        bullet_state = "ready"
-        score += 1
-        print(score)
-        enemy_x = random.randint(0, 736)
-        enemy_y = random.randint(64, 150)
-
     player_fn(player_x, player_y)
-    enemy_fn(enemy_x, enemy_y)
+    display_score(score_x, score_y)
     pg.display.update()
